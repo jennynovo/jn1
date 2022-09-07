c8c15026636b612690f69765f7e5dc618a7268ef
jennynovo <72864016+jennynovo@users.noreply.github.com>
2022-09-07 16:54:22 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Fuck Expo</Text>
+      <Text>text expo</Text>
       <StatusBar style="auto" />
     </View>
   );
